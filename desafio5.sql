76985aed88d584a5c230d193cec8f0f604710396
MateusHoffman <mateushoffmandev@gmail.com>
2022-09-23 21:35:31 -0300

    5 done

---
+++ desafio5.sql
@@ -0,0 +1,15 @@
+SELECT
+	DISTINCT so.song_name cancao,
+    COUNT(hi.song_id) reproducoes
+FROM SpotifyClone.history hi
+    JOIN SpotifyClone.songs so ON hi.song_id = so.song_id
+   GROUP BY so.song_id
+   ORDER BY reproducoes DESC, cancao ASC
+   LIMIT 2
+    
+-- Quais são as 2 músicas mais tocadas
+-- 2 colunas
+-- song AS cancao
+-- quantidade de pessoas que já escutou a música AS reproducoes
+-- ORDER BY número de reprodução DESC
+-- Caso de empate ordene pelo nome ASC
